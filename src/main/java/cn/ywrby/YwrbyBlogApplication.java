c116f06dfccea4a0fc7d916f4697fddef5703dbf
renboyu010214 <ywrby0214@gmail.com>
2021-04-23 12:28:11 +0800

    first commit

---
+++ src/main/java/cn/ywrby/YwrbyBlogApplication.java
@@ -0,0 +1,13 @@
+package cn.ywrby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YwrbyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YwrbyBlogApplication.class, args);
+    }
+
+}
